03273a109448e36918f6105d4dfadebdeb44b203
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2025-11-07 23:28:33 +0800

    Destructure product properties in map

--- 01-intro-react/src/components/ProductList.jsx
+++ 01-intro-react/src/components/ProductList.jsx
@@ -2,14 +2,14 @@ import {useState} from "react";
 import ProductCard from "./ProductCard";
 
 function ProductList() {
-    const [products, updateProduct] = useState([
+    const [products, setProducts] = useState([
         {id: 1, name: "Ice cream", price: 15, stock: "Available"},
         {id: 2, name: "Milk shake", price: 50, stock: "Unavailable"},
         {id: 3, name: "Oats", price: 20, stock: "Available"}
     ]);
 
     const handleProduct = (id, newStock) => {
-        updateProduct(products.map(product =>
+        setProducts(products.map(product =>
             product.id === id ? {...product, stock: newStock} : product
     ));
     }
@@ -17,13 +17,13 @@ function ProductList() {
     return (
         <div>
             <h2>Products List</h2>
-            {products.map(product => (
+            {products.map(({id, name, price, stock}) => (
                 <ProductCard 
-                key = {product.id}
-                id = {product.id}
-                name = {product.name}
-                price = {product.price}
-                stock = {product.stock}
+                key = {id}
+                id = {id}
+                name = {name}
+                price = {price}
+                stock = {stock}
                 onStatusChange = {handleProduct}/>
         ))}
         </div>
